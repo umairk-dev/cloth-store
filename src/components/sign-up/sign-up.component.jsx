4d91f7240a9cf737df1f3951b631de3d24e7ab24
Umair Khan <umair304@gmail.com>
2020-07-18 10:10:01 +1000

    implemented firebase utils, included ability to store authenticated users into our firebase database

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./sign-up.styles.scss";
 import FormInput from "../forn-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -11,17 +12,48 @@ class SignUp extends React.Component {
       displayName: "",
       email: "",
       password: "",
+      confirmPassword: "",
     };
   }
 
-  handleChange = (event) => {};
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserProfileDocument(user, { displayName });
+      this.state = {
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  handleChange = async (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
       <div className="sign-up">
         <h2>I don't have an account</h2>
         <span>Sign up with your email and password</span>
-        <form>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
           <FormInput
             name="displayName"
             type="text"
@@ -50,10 +82,10 @@ class SignUp extends React.Component {
           />
 
           <FormInput
-            name="password"
+            name="confirmPassword"
             type="password"
             label="Confirm Password"
-            value=""
+            value={this.state.confirmPassword}
             handleChange={this.handleChange}
             required
           />
